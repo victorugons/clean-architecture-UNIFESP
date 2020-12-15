39f9b71ec075f3dbe0cbecdadd2423fad288593c
victorugons <victorugons@gmail.com>
2020-12-14 23:54:24 -0300

    test: validate local part larger than 64 chars

--- src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
+++ src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
@@ -5,12 +5,19 @@ describe('Email validation', () => {
     const email = null
     expect(Email.validate(email)).toBeFalsy()
   })
+
   test('should not accept empty strings', () => {
     const email = ''
     expect(Email.validate(email)).toBeFalsy()
   })
+
   test('should accept valid e-mail', () => {
     const email = 'any@mail.com'
     expect(Email.validate(email)).toBeTruthy()
+  })
+
+  test('should not accept local part larger than 64 chars', () => {
+    const email = 'l'.repeat(65) + '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
   })
 })

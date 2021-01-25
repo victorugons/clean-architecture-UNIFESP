b29a034ddb5bfa66435dcaffdf15382cadf4f873
victorugons <victorugons@gmail.com>
2021-01-25 20:33:47 -0300

    refactor: simplify imports

--- src/entities/name.ts
+++ src/entities/name.ts
@@ -1,5 +1,5 @@
-import { Either, left, right } from '../shared/either'
-import { InvalidNameError } from './errors/invalid-name-error'
+import { Either, left, right } from '../shared'
+import { InvalidNameError } from './errors'
 
 export class Name {
     public readonly value: string
--- src/usecases/register-user-on-mailing-list/ports/user-repository.ts
+++ src/usecases/register-user-on-mailing-list/ports/user-repository.ts
@@ -1,4 +1,4 @@
-import { UserData } from '../../../entities/user-data'
+import { UserData } from '../../../entities'
 export interface UserRepository {
     add(user: UserData): Promise<void>
     findUserByEmail(email: String): Promise<UserData>

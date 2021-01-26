f12f190130cb71bd9e73bb3f56f740588875ce24
victorugons <victorugons@gmail.com>
2021-01-25 21:00:10 -0300

    refactor: use path mapping to improve imports

--- jest.config.js
+++ jest.config.js
@@ -18,5 +18,10 @@ module.exports = {
   testEnvironment: "node",
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@tests/(.*)$': '<rootDir>/tests/$1',
+
   }
 };

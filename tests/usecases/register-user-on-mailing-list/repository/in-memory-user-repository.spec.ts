189f68a5377309b633003f5979d1dbef3dfdc681
victorugons <victorugons@gmail.com>
2021-02-18 00:42:50 -0300

    refactor: move in memory user repository to source folder

--- tests/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ tests/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,5 +1,5 @@
 import { UserData } from '@/entities'
-import { InMemoryUserRepository } from '@tests/entities/usecases/register-user-on-mailing-list/repository'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
 
 describe('In memory User repository', () => {
   test('should return null if user is not found', async () => {
